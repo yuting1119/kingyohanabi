e36fd6c68cfc678b605e40a605d3f98259a7ab2a
洪于婷 <hungyuting@hongyutingdeMacBook-Pro.local>
2021-09-29 15:22:39 +0800

    (init)

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">商品一覽</router-link> |
+      <router-link to="/about">查詢訂單</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+@import './assets/all.scss'
+</style>
